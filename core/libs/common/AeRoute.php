853aca41fb882cb22d227153b19cf3e95b2fc776
Xavier Laumonier <xavier@Xavier-Laumoniers-MacBook-Air.local>
2011-09-16 09:13:15 +0200

    Adding AeRoute class

---
+++ core/libs/common/AeRoute.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * AeRoutes is the class that manage routes. It's used by <Dispatcher>.
+ *
+ * @see Dispatcher
+ */
+class AeRoute {
+
+    function parse ()
+    {
+	
+    }
+    
+    function get ( $tokens )
+    {
+	
+    }
+
+}
+
+?>
