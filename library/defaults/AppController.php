de449f99ba332e1e5ed32e9524a69468e931712f
Xavier Laumonier <dev@aenoa-systems.com>
2011-12-23 17:57:32 +0100

    Dev commit

---
+++ library/defaults/AppController.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Here is your AppController.
+ * 
+ * You should extends all your Controllers from this classe, and implement here methods that should be used in all your application.
+ */
+class AppController extends Controller {
+	
+	
+	
+	
+}
+
+?>
---
+++ library/defaults/AppModel.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Here is your AppModel.
+ * 
+ * You should extends all your Models from this classe, and implement here methods that should be used in all your application.
+ */
+class AppModel extends Model {
+	
+	
+	
+	
+}
+
+?>
