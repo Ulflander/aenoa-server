561b4e9b6e9bce8bcfc1dfb5d71aeeb2ebd24e4f
Xavier Laumonier <dev@aenoa-systems.com>
2012-02-02 19:13:10 +0100

    Fixes

--- dev-kit/tasks/utils/AjsfClassCreator.php
+++ dev-kit/tasks/utils/AjsfClassCreator.php
@@ -8,11 +8,17 @@ class AjsfClassCreator extends Task {
 	function process ()
 	{
 	    
+		
 		$template = new Template ( 'html/AjsfClassCreator.thtml' ) ;
 		$this->view->avoidMessages = true ;
-		$this->view->appendContent ( $template->render ( false ) ) ;
+		$this->view = $template ;
+		$this->view->layoutName = 'layout-backend' ;
+		
+	//	$this->view->appendContent ( $template->render ( false ) ) ;
 		
 		$this->view->render () ;
+		
+		App::end () ;
 	}
 	
 
